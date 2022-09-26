eef044e7ae1fed9a51b63d34cb5d86dc14303e70
Alec_Gracelli <alecgracelli@gmail.com>
2022-09-26 09:42:38 -0300

    create piramide

--- Desafio_Piramide/Program.cs
+++ Desafio_Piramide/Program.cs
@@ -24,7 +24,7 @@ internal class Program
                 }
 
                 linhas--;
-                cerca++;
+                cerca = cerca + 2;
                 Console.WriteLine();
             }
         else
